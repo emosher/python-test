2993c1d7c343a0edfa60aa3a99d2b9a4c5349161
Eric Mosher <emosher@users.noreply.github.com>
2023-05-17 15:19:14 -0400

    New version

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ DATA_URL = (
     "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
 )
 
-
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -22,9 +21,7 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-
 def seed_database():
-    # Connect to the Postgres database
     try:
         sb = binding.ServiceBinding()
         db_binding = sb.bindings("psql")
@@ -37,16 +34,13 @@ def seed_database():
         )
         cursor = conn.cursor()
 
-        # Check if the table has any data
         cursor.execute("SELECT COUNT(*) FROM uber;")
         result = cursor.fetchone()
         user_count = result[0]
 
-        # If the table is empty, seed the database
         if user_count == 0:
             data = load_data(10000)
 
-            # Create the table if it doesn't exist
             create_table_query = """
             CREATE TABLE IF NOT EXISTS uber (
                 -- Define your table columns here
@@ -54,43 +48,36 @@ def seed_database():
             """
             cursor.execute(create_table_query)
 
-            # Seed the database with the data
             insert_query = """
             INSERT INTO uber (column1, column2, ...)
             VALUES (%s, %s, ...);
             """
 
             for _, row in data.iterrows():
-                values = tuple(row["column1"], row["column2"], ...)  # Replace with actual column names
+                values = tuple(row[column] for column in data.columns)  # Get values from all columns
                 cursor.execute(insert_query, values)
 
-        # Commit the changes and close the connection
         conn.commit()
         cursor.close()
         conn.close()
+        return data
     except Exception as e:
         st.error("Error seeding the database: " + str(e))
+        return None
 
+data = seed_database()
 
-# Load data or seed the database if necessary
-try:
-    seed_database()
-    data_load_state = st.text("Data seeded into the database")
-except Exception as e:
-    data = load_data(10000)
-    data_load_state = st.text("Data loaded from the external source")
+if data:
+    if st.checkbox("Show raw data"):
+        st.subheader("Raw data")
+        st.write(data)
 
-if st.checkbox("Show raw data"):
-    st.subheader("Raw data")
-    st.write(data)
+    st.subheader("Number of pickups by hour")
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+    st.bar_chart(hist_values)
 
-st.subheader("Number of pickups by hour")
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.bar_chart(hist_values)
+    hour_to_filter = st.slider("hour", 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-# Some number in the range 0-23
-hour_to_filter = st.slider("hour", 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader("Map of all pickups at %s:00" % hour_to_filter)
-st.map(filtered_data)
+    st.subheader("Map of all pickups at %s:00" % hour_to_filter)
+    st.map(filtered_data)
